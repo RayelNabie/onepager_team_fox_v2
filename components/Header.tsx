2689f160d44aad7b581e5473b21852a0e88b1fdc
Rayel <rayelnabie@gmail.com>
2026-03-16 15:55:05 +0100

    feat: Add header and navbar, no Hamburgermenu yet

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__tagline">
+                <p>
+                    Samen hier. <br/> <em>Niet alleen online.</em>
+                </p>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
