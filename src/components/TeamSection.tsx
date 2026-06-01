98e6fa4b22bfd0d91f94cb6ccd4f1efdd102b36f
Rayel <rayelnabie@gmail.com>
2026-06-01 20:47:29 +0200

    chore: Update Linkedin links and add studentnumbers

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -5,35 +5,50 @@ import RayelImg from "@public/image_rayel.svg";
 import ShiteshImg from "@public/image_shitesh.svg";
 import Image from "next/image";
 import type React from "react";
-import type { JSX } from "react";
-import { LinkedinIcon } from "@/components/images/ImageLoader";
+import type {JSX} from "react";
+import {LinkedinIcon} from "@/components/images/ImageLoader";
 import type TeamMember from "@/types/TeamMemberInterface";
 
 const teamMembers: TeamMember[] = [
   {
     name: "Maureen van Eijk",
     role: "UX Designer",
+    studentID: "1032520",
     image: MaureenImg,
-    linkedin: "maureenvaneijk",
+    linkedinLink: "maureenvaneijk",
+    linkedinName: "maureenvaneijk",
   },
-  { name: "Rayel Nabie", role: "Software Developer", image: RayelImg, linkedin: "rayelnabie" },
+  {
+    name: "Rayel Nabie",
+    role: "Software Developer",
+    studentID: "1002893",
+    image: RayelImg,
+    linkedinLink: "rayelnabie",
+    linkedinName: "rayelnabie",
+  },
   {
     name: "Celine Scova Righini",
     role: "Data Scientist",
+    studentID: "1077277",
     image: CelineImg,
-    linkedin: "celine-s-righini",
+    linkedinLink: "celine-s-righini-647a16326",
+    linkedinName: "celine-s-righini",
   },
   {
     name: "Alejandro Dullemond",
     role: "Business & IT",
+    studentID: "1073316",
     image: AlejandroImg,
-    linkedin: "alejandrodullemond",
+    linkedinLink: "alejandro-dullemond-341ba4251",
+    linkedinName: "alejandro-dullemond",
   },
   {
     name: "Shitesh da Silva",
     role: "Software Developer",
+    studentID: "1083242",
     image: ShiteshImg,
-    linkedin: "shitesh-jay-da-silva",
+    linkedinLink: "shitesh-jay-da-silva-8a0782246",
+    linkedinName: "shitesh-jay-da-silva",
   },
 ];
 
@@ -49,23 +64,24 @@ const TeamSection: () => React.JSX.Element = (): JSX.Element => {
           {teamMembers.map(
             (member: TeamMember): JSX.Element => (
               <div className="team__member" key={member.name}>
-                  <Image
-                    src={member.image}
-                    alt={`Foto van ${member.name}`}
-                    className="team__image"
-                  />
+                <Image
+                  src={member.image}
+                  alt={`Foto van ${member.name}`}
+                  className="team__image"
+                />
                 <div className="team__info">
                   <h3 className="team__name">{member.name}</h3>
                   <p className="team__role">{member.role}</p>
+                  <p className="team__role">{member.studentID}</p>
 
                   <a
-                    href={`https://linkedin.com/in/${member.linkedin}`}
+                    href={`https://linkedin.com/in/${member.linkedinLink}`}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="team__linkedin"
                   >
-                    <LinkedinIcon className="team__linkedin-icon" />
-                    <span>{member.linkedin}</span>
+                    <LinkedinIcon className="team__linkedin-icon"/>
+                    <span>{member.linkedinName}</span>
                   </a>
                 </div>
               </div>
--- src/types/TeamMemberInterface.tsx
+++ src/types/TeamMemberInterface.tsx
@@ -3,6 +3,8 @@ import type { StaticImageData } from "next/image";
 export default interface TeamMember {
   name: string;
   role: string;
+  studentID: string;
   image: StaticImageData;
-  linkedin: string;
+  linkedinLink: string;
+  linkedinName: string;
 }
